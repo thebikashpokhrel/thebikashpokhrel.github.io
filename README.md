c5f9ee4b45dba1147fd27000b828c65e0d25244d
thebikash <thebikashpokhrel@gmail.com>
2022-04-12 21:57:27 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# thebikashpokhrel.github.io" 
---
+++ index.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-wrapper', 'particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
